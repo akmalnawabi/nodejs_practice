347699bffcc0ce0e3b7726c5ed94f998774595ce
Akmal Khan <akmalnawabi007@gmail.com>
2025-02-22 11:51:25 +0430

    new

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-const {readFile} = require('fs');
+const {readFile, writeFile} = require('fs').promises
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-const getText = (path) => {
-    return new Promise((reslove, reject) => {
-        readFile(path, 'utf8', (err, data) => {
-            if(err){
-                reject(err);
-            } else{
-                reslove(data);
-            }
-        })
-    })
+const start = async() => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile("./content/result.txt", 'utf8');
+        await writeFile('./content/new.txt', 'wow great')
+        console.log(first, second);
+    
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-getText('./content/first.txt')
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
+start()
+
+// const getText = (path) => {
+//     return new Promise((reslove, reject) => {
+//         readFile(path, 'utf8', (err, data) => {
+//             if(err){
+//                 reject(err);
+//             } else{
+//                 reslove(data);
+//             }
+//         })
+//     })
+// }
+// getText('./content/first.txt')
+//     .then(result => console.log(result))
+//     .catch(err => console.log(err))
