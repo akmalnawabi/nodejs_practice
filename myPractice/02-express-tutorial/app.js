b2d6fd3110ac27731a0ee21d0d47e14800981c3d
Akmal Khan <akmalnawabi007@gmail.com>
2025-03-01 15:59:53 +0430

    new

--- myPractice/02-express-tutorial/app.js
+++ myPractice/02-express-tutorial/app.js
@@ -1,50 +1,19 @@
-const http = require("http");
+const express = require('express');
+const app = express();
 
-const {readFileSync} = require("fs");
+const {products} = require('./data')
 
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyle = readFileSync("./navbar-app/styles.css");
-const homeLogo = readFileSync("./navbar-app/logo.svg");
-const homeJs = readFileSync("./navbar-app/browser-app.js");
+app.get('/', (req, res) => {
+  res.send('<h1>Home page</h1> <a href="/api/products">products<')
+})
 
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const {id, name, image} = product;
+    return {id, name, image}
+  })
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    res.writeHead("200", { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
+  res.json(newProducts)
+})
 
-  } else if (url === "/about") {
-    res.writeHead("200", { "content-type": "text/html" });
-    res.write("hello about page");
-    res.end();
-
-  } 
-  // styles
-  else if (url === "/styles.css") {
-    res.writeHead("200", { "content-type": "text/css" });
-    res.write(homeStyle);
-    res.end();
-  }
-  // logo image
-  else if (url === "/logo.svg") {
-    res.writeHead("200", { "content-type": "image/svg+xml" });
-    res.write(homeLogo);
-    res.end();
-  }
-  // js part
-  else if (url === "/browser-app.js") {
-    res.writeHead("200", { "content-type": "text/javascript" });
-    res.write(homeJs);
-    res.end();
-  } 
-  
-  else {
-    res.writeHead("404", { "content-type": "text/html" });
-    res.write("sorry page not found");
-    res.end();
-  }
-});
-
-server.listen(5000);
+app.listen('5000')
