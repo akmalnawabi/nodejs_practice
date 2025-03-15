2ecbad0ef75c9f4d43a6d4fe173262322f8011d2
Akmal Khan <akmalnawabi007@gmail.com>
2025-03-15 15:18:51 +0430

    new

--- myPractice/02-express-tutorial/app.js
+++ myPractice/02-express-tutorial/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-let {people} = require('./data')
+const people = require('./routes/people')
+const auth = require("./routes/auth");
 
 app.use(express.static('./methods-public'))
 
@@ -8,51 +9,9 @@ app.use(express.urlencoded({extended:false}))
 
 app.use(express.json())
 
-app.get('/api/people', (req,res) => {
-  res.status(200).json({success:true, data:people})
-})
+app.use('/api/people', people)
 
-app.post('/api/people', (req,res) => {
-  const {name} = req.body
-  if(!name){
-    return res.status(401).json({success:false, msg:'please enter a name'})
-  }
-  res.status(201).json({success:true, name:name})
-})
+app.use('/login', auth)
 
-app.post('/api/postman/people', (req,res)=>{
-  const {name} = req.body
-  if(!name){
-    return res.status(401).json({success:false, msg:'please enter a name'})
-  }
-  res.status(201).json({success:true, data:[...people,name]})
-})
-
-app.post('/login', (req,res) => {
-  const {name} = req.body
-
-  if(name){
-    res.status(200).send(`Welcome ${name}`)
-  }
-  res.status(401).send('pleade fill the form')
-})
-
-app.put('/api/people/:id', (req,res)=>{
-  const {id} = req.params
-  const {name} = req.body
-
-  const person = people.find((person)=>person.id === Number(id))
-
-  if(!person){
-    res.status(404).json({success:false, msg:`no person with id ${id}`})
-  }
-  const newPeople = people.map((person)=>{
-    if(person.id === Number(id)){
-      person.name = name
-    }
-    return person
-  })
-  res.json({success:true, data:newPeople})
-})
 
 app.listen('5000')
