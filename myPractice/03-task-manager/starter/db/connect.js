8e7f1dc04c2d0d8206928c59f6d137f2476c6b8d
Akmal Khan <akmalnawabi007@gmail.com>
2025-04-06 10:52:46 +0430

    new

--- myPractice/03-task-manager/starter/db/connect.js
+++ myPractice/03-task-manager/starter/db/connect.js
@@ -1,2 +1,9 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://akmal:<db_password>@cluster0.v17z1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://akmal:akmal123@cluster0.v17z1.mongodb.net/Task Manager?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("connected to the db"))
+  .catch((err) => console.log(err));
