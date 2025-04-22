b63cc335506dec6e7398f78ccfbf8c6dca718314
Akmal Khan <akmalnawabi007@gmail.com>
2025-04-22 14:59:55 +0430

    products

--- myPractice/04-store-api/starter/app.js
+++ myPractice/04-store-api/starter/app.js
@@ -1,1 +1,31 @@
-console.log('04 Store API')
+require('dotenv').config()
+const express = require('express')
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+const app = express()
+const connectDB = require('./db/connect')
+
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</hq><a href="/api/v1/products">products route</a>')
+})
+app.use('/api/v1/products')
+
+// product routes
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listen on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
---
+++ myPractice/04-store-api/starter/controllers/products.js
@@ -0,0 +1,9 @@
+
+
+const gitAllProductsStatic = async (req,res) => {
+    res.status(200).json({msg: 'products testing route'})
+}
+
+const gitAllProducts = async (req,res) => {
+    res.status(200).json({msg: "products route"})
+}
