be7a68994a804d25fd685e82879fc83990623ce2
Akmal Khan <akmalnawabi007@gmail.com>
2025-04-23 10:41:25 +0430

    new

--- myPractice/04-store-api/starter/app.js
+++ myPractice/04-store-api/starter/app.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
+require('express-async-errors')
 const express = require('express')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 const app = express()
 const connectDB = require('./db/connect')
+const productRouter = require('./routes/products')
 
 app.use(express.json())
 
@@ -11,7 +13,7 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('<h1>Store API</hq><a href="/api/v1/products">products route</a>')
 })
-app.use('/api/v1/products')
+app.use('/api/v1/products', productRouter)
 
 // product routes
 app.use(notFoundMiddleware)
