dc9d6a313240586022b0d9d3d51c56b608b1befb
Akmal Khan <akmalnawabi007@gmail.com>
2025-04-23 10:41:41 +0430

    new

---
+++ myPractice/04-store-api/starter/models/product.js
@@ -0,0 +1,34 @@
+const moongose = require('mongoose')
+
+const productSchema = moongose.Schema({
+  name: {
+    type: String,
+    required: [true, "name is required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "price is required"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    name: String,
+    enum: {
+      values: ["benz", "BMW"],
+      message: '{VALUE} is not supported'
+    },
+    // enum: ['benz', 'BMW']
+  },
+});
+
+module.exports = moongose.model('Product', productSchema)
