c8cc77bbeaf0e9fd94c284c897464c6e7e04c776
Akmal Khan <akmalnawabi007@gmail.com>
2025-04-23 10:41:36 +0430

    new

--- myPractice/04-store-api/starter/controllers/products.js
+++ myPractice/04-store-api/starter/controllers/products.js
@@ -1,9 +1,15 @@
 
 
 const gitAllProductsStatic = async (req,res) => {
+    // throw new Error("testing async errors")
     res.status(200).json({msg: 'products testing route'})
 }
 
 const gitAllProducts = async (req,res) => {
     res.status(200).json({msg: "products route"})
+}
+
+module.exports = {
+    gitAllProducts,
+    gitAllProductsStatic
 }
